671274d114c6b901409a1bb6595d28eae7edcbb9
Pablo Nicolas Nieva <geometry@AUM.local>
2023-01-14 14:43:01 +0100

    deploy

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5003', {
+    const response = await fetch('https://testing-openai.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
